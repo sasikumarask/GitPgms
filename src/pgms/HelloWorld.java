e8bb3f658d7bc3b820ff84a3ac2926a91b6223c9
sasikumarask <159776092+sasikumarask@users.noreply.github.com>
2025-02-28 20:36:09 +0530

    Update HelloWorld.java

--- src/pgms/HelloWorld.java
+++ src/pgms/HelloWorld.java
@@ -2,8 +2,8 @@ package pgms;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		String str="HelloWorld";
-		System.out.println(str);
+		String str1="HelloWorld";
+		System.out.println(str1);
 
 	}
 
