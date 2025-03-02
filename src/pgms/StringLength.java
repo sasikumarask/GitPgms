b23df79f1c8f40e986b81802a959d7d6fd0718e6
sasikumarask <sasiaruna1526@gmail.com>
2025-03-02 22:42:42 +0530

    String Length

---
+++ src/pgms/StringLength.java
@@ -0,0 +1,12 @@
+package pgms;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		String str="hello";
+		int len=str.length();
+		System.out.println("Length of the String is: "+len);
+
+	}
+
+}
